a51a2b66d1a0b96cc558ee886676da3669ef0f87
Tobias Ritter <tobias.ritter@tum.de>
2022-02-26 15:37:58 +0100

    remove presets form entropy

--- entropy_gain.py
+++ entropy_gain.py
@@ -20,34 +20,7 @@ def gain_ratio(prev_split: list[list[int]], new_split: list[list[int]]) -> float
 
 
 def main():
-    # print(entropy([0.1, 0.9]))
-    # print(entropy([0.2, 0.8]))
-    # print(entropy([0.3, 0.7]))
-    # print(entropy([0.5, 0.5]))
-    # print(entropy([0.1, 0.1, 0.8]))
-
-    # print(info([[2, 3]]))
-    # print(info([[5, 4]]))
-    # print(info([[2, 3], [5, 4]]))
-    # print(info([[2, 3], [9, 0]]))
-
-    # print(gain([[5, 5]], [[2, 3], [3, 1]]))  # temp
-    # print(gain([[5, 5]], [[3, 0], [0, 3], [2, 1]]))  # vis
-    # print(gain([[5, 5]], [[1, 3], [4, 1]]))  # depth
-
-    # print(gain([[2, 1]], [[0, 1], [2, 0]]))  # depth
-    # print(gain([[2, 1]], [[1, 1], [1, 0]]))  # temp
-
-    # print(gain_ratio([[5, 5]], [[2, 3], [3, 1]]))  # temp
-    # print(gain_ratio([[5, 5]], [[3, 0], [0, 3], [2, 1]]))  # vis
-    # print(gain_ratio([[5, 5]], [[1, 3], [4, 1]]))  # depth
-
-    # print(gain_ratio([[2, 1]], [[0, 1], [2, 0]]))  # depth
-    # print(gain_ratio([[2, 1]], [[1, 1], [1, 0]]))  # temp
-
-    print(info([[2,0],[3,3]]))
-    print(info([[4,1],[1,2]]))
-    print(info([[5,1],[0,2]]))
+    pass
 
 
 if __name__ == "__main__":
